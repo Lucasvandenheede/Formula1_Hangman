f40c3b7889c8079bba12422f8da8d695b6c5c847
LucasVandenHeede <r0950888@student.thomasmore.be>
2023-12-17 01:19:35 +0100

    Finished

--- F1Hangman/Form1.Designer.cs
+++ F1Hangman/Form1.Designer.cs
@@ -30,46 +30,113 @@ namespace F1Hangman
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(347, 70);
+            this.button1.Location = new System.Drawing.Point(628, 382);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(106, 34);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Start New Game";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // dataGridView1
+            // textBox1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(261, 181);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 1;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(589, 240);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(644, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Guess";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(155, 135);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(428, 281);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(155, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(428, 37);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Hidden Word";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.Title.Location = new System.Drawing.Point(262, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(214, 39);
+            this.Title.TabIndex = 7;
+            this.Title.Text = "F1 Hangman";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(659, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Correct Word";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- F1Hangman/Form1.cs
+++ F1Hangman/Form1.cs
@@ -1,37 +1,179 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Data.SQLite;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace F1Hangman
 {
     public partial class Form1 : Form
     {
+        private string correctWord;
+        private string hiddenWord;
+        private int incorrectGuesses;
+        private List<string> Guesses = new List<string>();
+
         public Form1()
         {
             InitializeComponent();
+            generateHiddenWord();
+        }
+
+        private void generateHiddenWord() // Function to generate a hidden word
+        {
+            // Connection to database
+            SQLiteConnection connection = new SQLiteConnection(@"data source=C:\Users\lucas\Thomas more\2APPAI02\Devops\CaseStudy\F1Hangman\Database\F1woorden.db");
+
+            // Open connection
+            connection.Open();
+
+            // Create command object
+            string query = "SELECT * FROM Formula1";
+            SQLiteCommand cmd = new SQLiteCommand(query, connection);
+
+            // Execute query and get data reader
+            SQLiteDataReader reader = cmd.ExecuteReader();
+
+            // Create a list to store the words
+            List<string> words = new List<string>();
+
+            // Read all the words from the data reader and add them to the list
+            while (reader.Read())
+            {
+                words.Add(reader["woorden"].ToString());
+            }
+
+            // Close the data reader and connection
+            reader.Close();
+            connection.Close();
+
+            // Create the hidden word from the database
+            int randomIndex = new Random().Next(0, words.Count);
+            correctWord = words[randomIndex].ToLower();
+            hiddenWord = "";
+
+            // Replacing each letter with "_"
+            for (int i = 0; i < correctWord.Length; i++)
+            {
+                char currentChar = words[randomIndex][i];
+                if (char.IsWhiteSpace(currentChar))
+                {
+                    hiddenWord += ' ';
+                }
+                else
+                {
+                    hiddenWord += '.';
+                }
+            }
+            // Displaying hidden word
+            label2.Text = hiddenWord;
+            label1.Text = correctWord; // Showing for testing purpose
+
+            // Set incorrect guesses to 0
+            incorrectGuesses = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Guess button
+
+            // Retrieves the user's guess from the text box and set it to lowercase
+            String guess = textBox1.Text.ToLower();
+
+            // Validate the user's guess
+            if (guess == correctWord)
+            {
+                label2.Text = correctWord;
+                pictureBox1.Image = new Bitmap(@"C:\Users\lucas\Thomas more\2APPAI02\Devops\CaseStudy\F1Hangman\Images\win.png");
+                MessageBox.Show("Congratulations! You have won the Hangman Grand Prix!");
+                textBox1.Text = "";
+            }
+            else if (Guesses.Contains(guess)) // If player enters letter already guessed
+            {
+                MessageBox.Show("You've already guessed this letter");
+                textBox1.Text = "";
+            }
+            else if (correctWord.Contains(guess) && guess.Length == 1) // Check if the guess is correct
+            {
+                // If the guess is correct
+                for (int i = 0; i < correctWord.Length; i++)
+                {
+                    if (correctWord[i].ToString() == guess)
+                    {
+                        // Replace the underscore at the correct position with the guessed letter
+                        label2.Text = label2.Text.Substring(0, i) + guess + label2.Text.Substring(i + 1);
+                    }
+                }
+
+                // Check for win condition
+                if (label2.Text == correctWord)
+                {
+                    pictureBox1.Image = new Bitmap(@"C:\Users\lucas\Thomas more\2APPAI02\Devops\CaseStudy\F1Hangman\Images\win.png");
+                    MessageBox.Show("Congratulations! You have won the Hangman Grand Prix!");
+                    label2.Text = correctWord;
+                }
+                textBox1.Text = "";
+
+                // Add the guess to the list of guesses
+                Guesses.Add(guess);
+            }
+            else
+            {
+                if (!Guesses.Contains(guess) || guess != correctWord) // If guess is not correct
+                {
+                    // If the guess is incorrect, update the incorrect guesses counter
+                    incorrectGuesses++;
+
+                    // Show message if guess is incorrect
+                    if (incorrectGuesses < 5)
+                    {
+                        MessageBox.Show($"The stewards are watching closely. You have {5 - incorrectGuesses} track limits left before you're black-flagged.");
+                    }
+
+                    // Reset the textbox
+                    textBox1.Text = "";
+
+                    // Update the picture of the hangman
+                    if (incorrectGuesses == 1)
+                    {
+                        pictureBox1.Image = new Bitmap(@"C:\Users\lucas\Thomas more\2APPAI02\Devops\CaseStudy\F1Hangman\Images\1.png");
+                    }
+                    else if (incorrectGuesses == 2)
+                    {
+                        pictureBox1.Image = new Bitmap(@"C:\Users\lucas\Thomas more\2APPAI02\Devops\CaseStudy\F1Hangman\Images\2.png");
+                    }
+                    else if (incorrectGuesses == 3)
+                    {
+                        pictureBox1.Image = new Bitmap(@"C:\Users\lucas\Thomas more\2APPAI02\Devops\CaseStudy\F1Hangman\Images\3.png");
+                    }
+                    else if (incorrectGuesses == 4)
+                    {
+                        pictureBox1.Image = new Bitmap(@"C:\Users\lucas\Thomas more\2APPAI02\Devops\CaseStudy\F1Hangman\Images\4.png");
+                    }
+                    else if (incorrectGuesses == 5)
+                    {
+                        pictureBox1.Image = new Bitmap(@"C:\Users\lucas\Thomas more\2APPAI02\Devops\CaseStudy\F1Hangman\Images\crash.png");
+                        label2.Text = correctWord;
+                        MessageBox.Show($"You have pushed too hard, you have have crashed.\nThe word was: {correctWord}");
+                    }
+
+                    // Add the incorrect guess to the list of guesses
+                    Guesses.Add(guess);
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Connection object
-            SQLiteConnection con = new SQLiteConnection(@"data source=C:\Users\lucas\Thomas more\2APPAI02\Devops\CaseStudy\database\F1woorden.db");
-            // commond object
-            string query = "SELECT * FROM Formula1";
-            SQLiteCommand cmd = new SQLiteCommand(query, con);
-            // adapter
-            // datatable
-            DataTable dt = new DataTable();
-            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
-            adapter.Fill(dt);
+            // Start new game
+            // Reset everything
+            textBox1.Text = ""; // Clears the text box
+            pictureBox1.Image = null; // Clears the picture bow
+            label2.Text = ""; // Clears the hidden word
+            Guesses.Clear(); // Clears the list of guesses
 
-            dataGridView1.DataSource = dt;
+            // Generate new word
+            generateHiddenWord();
         }
     }
 }
